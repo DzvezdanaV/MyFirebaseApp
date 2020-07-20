2d4909a023b434f6943839bf846e19b1cbe46810
Velkovska89 <velkovska89@gmail.com>
2020-07-20 19:45:53 +0200

    Initial commit

---
+++ app/src/main/java/com/gsixacademy/android/myfirebaseapp/ListActivity.kt
@@ -0,0 +1,18 @@
+package com.gsixacademy.android.myfirebaseapp
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_list.*
+
+class ListActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val listAdapter = ListAdapter(ArrayList())
+        recycler_view_main.adapter = listAdapter
+
+
+    }
+    }
